a45345a0b8b95420ee896440680dba01d149e8e2
kmkaliraj <kalirajkalimuthu@kalirajs-macbook-pro.local>
2019-03-19 19:30:52 -0700

    Initial Commit

---
+++ InstaHappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaHappy
+//
+//  Created by Kaliraj Kalimuthu on 3/19/19.
+//  Copyright © 2019 Shravan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
